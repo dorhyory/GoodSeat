21cf0f534fe6057f2aa8b2f77c8c415fecf309f3
dorhyory <dorhyory@outlook.com>
2020-03-13 00:35:40 +0900

    added random function to index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-var listLength = 32;
 var elementlist;
-var namelist = ["123", "456", "789"];
-var genderlist = ["m", "m", "w"];
+var namelist = ["123", "456", "789", "aaa", "bbb"];
+
 function setRandomSeat() {
   elementlist = document.querySelectorAll("#class_seat li");
+  
+  var rdArray = [];
+  var rdResult;
+  while(true) {
+    rdResult = Math.floor(Math.random()*namelist.length);
+    if(rdArray.indexOf(rdResult) === -1) {
+      rdArray.push(rdResult);
+      if(rdArray.length === namelist.length) break;
+    }
+  }
 
+  for(var i in rdArray) {
+    elementlist[i].innerHTML = namelist[rdArray[i]];
+  }
 }
