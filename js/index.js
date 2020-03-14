7a73fb10df4e6748349bdd7c37610eb112a826f0
dorhyory <dorhyory@outlook.com>
2020-03-14 15:06:55 +0900

    applied gender system to index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var elementlist;
-var namelist = ["123", "456", "789", "aaa", "bbb"];
+var pArray = [];
 
 function setRandomSeat() {
   elementlist = document.querySelectorAll("#class_seat li");
@@ -7,14 +7,37 @@ function setRandomSeat() {
   var rdArray = [];
   var rdResult;
   while(true) {
-    rdResult = Math.floor(Math.random()*namelist.length);
+    rdResult = Math.floor(Math.random()*pArray.length);
     if(rdArray.indexOf(rdResult) === -1) {
       rdArray.push(rdResult);
-      if(rdArray.length === namelist.length) break;
+      if(rdArray.length === pArray.length) break;
     }
   }
 
   for(var i in rdArray) {
-    elementlist[i].innerHTML = namelist[rdArray[i]];
+    elementlist[i].innerHTML = pArray[rdArray[i]].name;
   }
+}
+function Person(name, gender) {
+  this.name = name;
+  this.gender = gender;
+}
+function setPerson() {
+  var getNameElement = document.getElementById("nameText");
+  var getGenderM = document.getElementById("genderM");
+  var getGenderW = document.getElementById("genderW");
+  var getName = getNameElement.value;
+  if(getName != "") {
+    if(getGenderM.checked == true) {
+      pArray.push(new Person(getName, "m"));
+    } else if(getGenderW.checked == true) {
+      pArray.push(new Person(getName, "w"));
+    } else {
+      alert("gender not checked");
+    }
+  } else {
+    alert("name not typed");
+  }
+  
+  getNameElement.value = "";
 }
